a7d1396e7ca905237cee8920195f4fc02ae19d9c
Janet Bello <bellojanet022@gmail.com>
2024-02-13 21:27:50 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import MainSections from "./product-page/Sections.jsx";
 
 function App() {
   return (
-    <div className=" flex justify-center ">
+    <div className=" flex  ">
       <div className=" flex flex-col w-[1200px] border-red-700">
         <Header />
         <hr className=" md:w-full " />
-        <div className="mt-20">
+        <div className=" md:mt-20">
           <MainSections />
         </div>
       </div>
